f56e87606bf7c249b419469c0fefa514a5c6b839
Devaurion <devaurion@gmail.com>
2019-08-01 14:08:54 +0700

    Feature component and import featured to index

---
+++ src/components/Home/FeaturedTours.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Tour from "../Tours/Tour"
+
+const FeaturedTours = () => {
+  return (
+    <div>
+      Hellow from feature tours
+      <Tour />
+    </div>
+  )
+}
+
+export default FeaturedTours
---
+++ src/components/Tours/Tour.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Tour = () => {
+  return <div>This is a single tour</div>
+}
+
+export default Tour
